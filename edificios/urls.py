3ca093191e4c89ac4adc23390cf9bbe5d7c011b9
Mauro Pisani Curlo <mauro903_7@hotmail.com>
2023-01-15 01:30:31 -0300

    Se agrega en settings el app edificios, se agregan HTML inquilinos y se agregan cambios al html base

--- edificios/urls.py
+++ edificios/urls.py
@@ -1,7 +1,8 @@
-from edificios.views import bienvenidos
+from edificios.views import bienvenidos, listar_inquilinos
 from django.urls import path
 
 
 urlpatterns = [
     path("bienvenidos/", bienvenidos),
+    path("inquilinos/", listar_inquilinos),
 ]
--- edificios/views.py
+++ edificios/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def bienvenidos(request):
     return HttpResponse("Bienvenidos a consorcios mengano")
 # Create your views here.
+
+def listar_inquilinos(request):
+    return render(request=request, template_name="edificios/lista_inquilinos.html")
+    
